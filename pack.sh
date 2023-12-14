aa425e7ff0992b9c425e85679996e57bd180a2f7
Bob Dickinson <bob.dickinson@gmail.com>
2023-12-13 17:42:30 -0800

    Packed without env

--- pack.sh
+++ pack.sh
@@ -7,14 +7,18 @@ tmpdir=$(mktemp -d -t ci-XXXXXXXXXX)
 
 # Use rsync to copy all files and directories to the temporary directory,
 # while respecting .funcignore
-rsync -av --prune-empty-dirs --exclude-from=.funcignore ./ $tmpdir/
+rsync -a --prune-empty-dirs --exclude-from=.funcignore ./ $tmpdir/
 
 python3 -m venv env
 source env/bin/activate
 
 pip install  --target="$tmpdir/.python_packages/lib/site-packages" -r requirements.txt
 
+# Change permissions of the installed packages
+chmod -R u+rwX "$tmpdir/.python_packages"
+
 deactivate
+rm -rf ./env
 
 # Create the dist directory if it doesn't exist
 mkdir -p ./dist
@@ -22,7 +26,7 @@ mkdir -p ./dist
 # Use zip to create a zip file in the dist directory from the temporary directory
 cd $tmpdir
 rm -f $OLDPWD/dist/censys-sentinel.zip
-zip -r $OLDPWD/dist/censys-sentinel.zip .
+zip -r $OLDPWD/dist/censys-sentinel.zip . -x "*.DS_Store" > /dev/null
 
 # Remove the temporary directory
 cd ..
